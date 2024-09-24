005a07ed90496d72dab7754d0b7f020543b8d089
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-09-24 13:04:14 +0300

    Revert ""Updated UserComponent to use computed properties and signals from @angular/core""
    This reverts commit 0412b470e8203969a9775a7ef358318fb30c54ae.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,17 +8,12 @@ import { Component, computed, input, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  });
-  // imagePath() {
-  //   return `assets/users/${this.avatar}`;
-  // }
+  imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
 
   onSelectUSer() {}
 }
