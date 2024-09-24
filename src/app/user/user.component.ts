f754d91d301c4240d115fa44f206eced883c34b1
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-09-24 12:51:16 +0300

    "Refactor user component to accept avatar and name as inputs, and update app component to pass user data to user component"

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-user';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,14 +8,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-  // public get imagePath(): string {
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  onSelectUSer() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  imagePath() {
+    return `assets/users/${this.avatar}`;
   }
+
+  onSelectUSer() {}
 }
