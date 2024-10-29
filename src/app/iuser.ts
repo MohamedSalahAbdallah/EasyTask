ed3d24338bd8340211f2a849fe0ea8dc991e8ecf
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-10-29 23:32:42 +0300

    "Updated app component HTML, added selected user ID to user component, modified tasks component to filter tasks by user ID, and made various other changes to component templates and classes."

---
+++ src/app/iuser.ts
@@ -0,0 +1,5 @@
+export interface IUSer {
+  id: string;
+  avatar: string;
+  name: string;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IUSer } from '../iuser';
 
 @Component({
   selector: 'app-user',
@@ -11,11 +12,8 @@ export class UserComponent {
   // @Input({ required: true }) id!: string;
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
-  @Input({ required: true }) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({ required: true }) user!: IUSer;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter();
 
   imagePath() {
