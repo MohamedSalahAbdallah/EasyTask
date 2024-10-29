0c6e65c9232ffda8f0616522474fb24f3385d5e5
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-10-29 22:34:46 +0300

    "Refactor user component to accept a single 'user' object as input, simplify app component template, and add fallback message when no user is selected."

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { TasksComponent } from './tasks/tasks.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId: string = this.users[0].id;
+  selectedUserId?: string;
 
   public get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
